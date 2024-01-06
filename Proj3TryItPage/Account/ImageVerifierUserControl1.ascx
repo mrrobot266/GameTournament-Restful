72dfeeddf07d4e2204cd3186388a675509a592ad
Erick Soto <ericksoto266@gmail.com>
2024-01-06 12:53:05 -0700

    Add files via upload

---
+++ Proj3TryItPage/Account/ImageVerifierUserControl1.ascx
@@ -0,0 +1,12 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ImageVerifierUserControl1.ascx.cs" Inherits="Proj3TryItPage.ImageVerifierUserControl1" %>
+<p>
+    <asp:Image ID="Image1" runat="server" Height="68px" Width="147px" />
+    <asp:Button ID="imgBtn" runat="server" OnClick="imgBtn_Click" style="margin-left: 25px" Text="Get New Image" />
+</p>
+<p>
+    <span style="color: rgb(0, 0, 0); font-family: &quot;Times New Roman&quot;; font-size: medium; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;">Please enter the text above into the following textbox<asp:Label ID="errorLbl" runat="server" ForeColor="#FF3300" Text=" *Text does not match image" Visible="False"></asp:Label>
+    </span>
+</p>
+<p>
+    &nbsp;</p>
+
---
+++ Proj3TryItPage/Account/TournRegUC.ascx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace Proj3TryItPage.Account
+{
+    public partial class TournRegUC : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+
+
+            string filepath = HttpRuntime.AppDomainAppPath + @"App_Data\FifaRegistered.xml";
+            string username = TextBox2.Text;
+            string email = TextBox3.Text;
+
+            XmlDocument myDoc = new XmlDocument();
+            myDoc.Load(filepath); // open file
+            XmlElement rootElement = myDoc.DocumentElement;
+
+            int maxlimit = 0;
+
+            foreach (XmlNode node in rootElement.ChildNodes)
+            {
+                maxlimit++;
+                if (maxlimit >= 8)
+                {
+                    Label5.Text = "*Maximum number of players have registered*";
+                    Label5.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+
+            if (maxlimit < 8)
+            {
+                XmlElement myMember = myDoc.CreateElement("Member",
+                    rootElement.NamespaceURI);
+                rootElement.AppendChild(myMember);
+                XmlElement myUser = myDoc.CreateElement("Username",
+                rootElement.NamespaceURI);
+                myMember.AppendChild(myUser);
+                myUser.InnerText = username;
+                XmlElement myEmail = myDoc.CreateElement("Email",
+                rootElement.NamespaceURI);
+                myMember.AppendChild(myEmail);
+                myEmail.InnerText = email;
+                myDoc.Save(filepath);
+            }
+
+
+
+            
+                Label5.Visible = true;
+            
+        }
+    }
+}
